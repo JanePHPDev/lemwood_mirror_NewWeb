17b6f6dc44daa5cab46aa958373bce2e4ef3e023
JanePHPDev <administer@zeinklab.com>
2026-01-30 12:51:15 +0800

    修复了一些已知问题。

--- web/src/components/layout/Sidebar.vue
+++ web/src/components/layout/Sidebar.vue
@@ -45,7 +45,7 @@ const links = [
     </div>
     <div class="mt-auto p-4">
         <div class="text-xs text-muted-foreground text-center">
-            v3.14.6
+            v3.14.7
         </div>
     </div>
   </div>
